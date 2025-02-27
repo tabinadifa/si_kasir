2de6081fdc69177aab74a11c850551a78d1d53b8
tabinadifa <tatadd.235@gmail.com>
2025-02-27 19:54:09 +0700

    total saldo

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -5,8 +5,10 @@ import 'package:si_kasir/views/toko/read_toko.dart';
 import 'package:si_kasir/views/kasir/daftar_produk.dart';
 import 'package:si_kasir/views/laporan/laporan.dart';
 import 'package:si_kasir/views/toko/create_toko.dart';
+import 'package:si_kasir/views/laporan/totaltransaksi.dart';
 import 'package:si_kasir/views/transaksi/transaksi.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:intl/intl.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -16,15 +18,58 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  String profileImageUrl = ''; // Variabel untuk menyimpan URL gambar profil
-  bool isLoadingProfile = true; // Untuk menangani loading state
+  String profileImageUrl = ''; 
+  bool isLoadingProfile = true; 
+  double totalSaldo = 0.0;
 
   @override
   void initState() {
     super.initState();
-    _loadProfileData(); // Panggil fungsi untuk memuat data profil
+    _loadProfileData(); 
+    _loadSaldoData();
   }
 
+  String formatCurrency(double amount) {
+    final format = NumberFormat.currency(
+      locale: 'id_ID', 
+      symbol: 'Rp', 
+      decimalDigits: 0, 
+    );
+    return format.format(amount);
+  }
+
+  Future<void> _loadSaldoData() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+      if (user == null) {
+        setState(() {
+          isLoadingProfile = false;
+        });
+        return;
+      }
+
+      final String emailUser = user.email!;
+      final transaksiRef = FirebaseFirestore.instance.collection('transaksi');
+      final querySnapshot = await transaksiRef.where('email', isEqualTo: emailUser).get();
+
+      double total = 0.0;
+
+      for (var doc in querySnapshot.docs) {
+        final data = doc.data();
+        final cashAmount = data['cashAmount'] ?? 0.0;
+        final initialPayment = data['initialPayment'] ?? 0.0;
+
+        // Jika cashAmount 0, gunakan initialPayment
+        total += cashAmount != 0.0 ? cashAmount : initialPayment;
+      }
+
+      setState(() {
+        totalSaldo = total;
+      });
+    } catch (e) {
+      print('Error loading saldo data: $e');
+    }
+  }
   Future<void> _loadProfileData() async {
     try {
       final user = FirebaseAuth.instance.currentUser;
@@ -124,132 +169,206 @@ class _DashboardScreenState extends State<DashboardScreen> {
     }
   }
 
-  Widget _buildHeader(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
-    final isSmallScreen = screenWidth < 360;
+Widget _buildHeader(BuildContext context) {
+  final screenWidth = MediaQuery.of(context).size.width;
+  final screenHeight = MediaQuery.of(context).size.height;
+  final isSmallScreen = screenWidth < 360;
 
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: screenWidth * 0.05,
-        vertical: screenHeight * 0.02,
+  return Container(
+    padding: EdgeInsets.symmetric(
+      horizontal: screenWidth * 0.05,
+      vertical: screenHeight * 0.02,
+    ),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: const BorderRadius.only(
+        bottomLeft: Radius.circular(30),
+        bottomRight: Radius.circular(30),
       ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(30),
-          bottomRight: Radius.circular(30),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.1),
+          spreadRadius: 0,
+          blurRadius: 10,
+          offset: const Offset(0, 4),
         ),
-        boxShadow: [
-          BoxShadow(
-            // ignore: deprecated_member_use
-            color: Colors.grey.withOpacity(0.1),
-            spreadRadius: 0,
-            blurRadius: 10,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              GestureDetector(
-                onTap: () async => await _navigateToToko(context),
-                child: Container(
-                  padding: const EdgeInsets.all(3),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: const Color(0xFF133E87),
-                      width: 2,
-                    ),
-                  ),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.grey.shade100,
-                    radius: isSmallScreen ? 25 : 30,
-                    child: isLoadingProfile
-                        ? CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                                const Color(0xFF133E87)),
-                          )
-                        : profileImageUrl.isNotEmpty
-                            ? ClipOval(
-                                child: Image.network(
-                                  profileImageUrl,
-                                  fit: BoxFit.cover,
-                                  width: isSmallScreen ? 50 : 60,
-                                  height: isSmallScreen ? 50 : 60,
-                                  loadingBuilder: (context, child, loadingProgress) {
-                                    if (loadingProgress == null) return child;
-                                    return Center(
-                                      child: CircularProgressIndicator(
-                                        value: loadingProgress.expectedTotalBytes !=
-                                                null
-                                            ? loadingProgress.cumulativeBytesLoaded /
-                                                loadingProgress.expectedTotalBytes!
-                                            : null,
-                                      ),
-                                    );
-                                  },
-                                  errorBuilder: (context, error, stackTrace) {
-                                    return Icon(
-                                      Icons.person,
-                                      color: const Color(0xFF133E87),
-                                      size: isSmallScreen ? 28 : 32,
-                                    );
-                                  },
-                                ),
-                              )
-                            : Icon(
-                                Icons.person,
-                                color: const Color(0xFF133E87),
-                                size: isSmallScreen ? 28 : 32,
-                              ),
+      ],
+    ),
+    child: Column(
+      children: [
+        Row(
+          children: [
+            GestureDetector(
+              onTap: () async => await _navigateToToko(context),
+              child: Container(
+                padding: const EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: const Color(0xFF133E87),
+                    width: 2,
                   ),
                 ),
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey.shade100,
+                  radius: isSmallScreen ? 25 : 30,
+                  child: isLoadingProfile
+                      ? CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              const Color(0xFF133E87)),
+                        )
+                      : profileImageUrl.isNotEmpty
+                          ? ClipOval(
+                              child: Image.network(
+                                profileImageUrl,
+                                fit: BoxFit.cover,
+                                width: isSmallScreen ? 50 : 60,
+                                height: isSmallScreen ? 50 : 60,
+                                loadingBuilder: (context, child, loadingProgress) {
+                                  if (loadingProgress == null) return child;
+                                  return Center(
+                                    child: CircularProgressIndicator(
+                                      value: loadingProgress.expectedTotalBytes !=
+                                              null
+                                          ? loadingProgress.cumulativeBytesLoaded /
+                                              loadingProgress.expectedTotalBytes!
+                                          : null,
+                                    ),
+                                  );
+                                },
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Icon(
+                                    Icons.person,
+                                    color: const Color(0xFF133E87),
+                                    size: isSmallScreen ? 28 : 32,
+                                  );
+                                },
+                              ),
+                            )
+                          : Icon(
+                              Icons.person,
+                              color: const Color(0xFF133E87),
+                              size: isSmallScreen ? 28 : 32,
+                            ),
+                ),
               ),
-              SizedBox(width: screenWidth * 0.04),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Halo, Selamat Datang!',
-                      style: TextStyle(
-                        fontSize: isSmallScreen ? 18 : 20,
-                        fontWeight: FontWeight.bold,
-                        color: const Color(0xFF133E87),
+            ),
+            SizedBox(width: screenWidth * 0.04),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Halo, Selamat Datang!',
+                    style: TextStyle(
+                      fontSize: isSmallScreen ? 18 : 20,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFF133E87),
+                    ),
+                  ),
+                  SizedBox(height: screenHeight * 0.01),
+                  ElevatedButton(
+                    onPressed: _launchHelpCenter,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF133E87),
+                      elevation: 0,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: isSmallScreen ? 12 : 20,
+                        vertical: isSmallScreen ? 8 : 12,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
                       ),
                     ),
-                    SizedBox(height: screenHeight * 0.01),
-                    ElevatedButton(
-                      onPressed: _launchHelpCenter,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF133E87),
-                        elevation: 0,
-                        padding: EdgeInsets.symmetric(
-                          horizontal: isSmallScreen ? 12 : 20,
-                          vertical: isSmallScreen ? 8 : 12,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(Icons.help_outline, color: Colors.white),
-                          SizedBox(width: screenWidth * 0.02),
-                          Text(
-                            "Pusat Bantuan",
-                            style: TextStyle(
-                              fontSize: isSmallScreen ? 12 : 14,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w600,
-                            ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.help_outline, color: Colors.white),
+                        SizedBox(width: screenWidth * 0.02),
+                        Text(
+                          "Pusat Bantuan",
+                          style: TextStyle(
+                            fontSize: isSmallScreen ? 12 : 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
                           ),
-                        ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: screenHeight * 0.02),
+        Container(
+          padding: EdgeInsets.all(screenWidth * 0.05),
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [Color(0xFF133E87), Color(0xFF1E56B1)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Total Saldo',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.9),
+                      fontSize: isSmallScreen ? 12 : 14),
+                  ),
+                  SizedBox(height: screenHeight * 0.01),
+                  Text(
+                    formatCurrency(totalSaldo), // Format total saldo
+                    style: TextStyle(
+                      fontSize: isSmallScreen ? 20 : 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                      MaterialPageRoute(
+                        builder: (context) => TotalTransaksiScreen(), 
+                      ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  elevation: 0,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: isSmallScreen ? 12 : 20,
+                    vertical: isSmallScreen ? 8 : 12,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.account_balance_wallet,
+                      color: const Color(0xFF133E87),
+                    ),
+                    SizedBox(width: screenWidth * 0.02),
+                    Text(
+                      'Dompet',
+                      style: TextStyle(
+                        fontSize: isSmallScreen ? 12 : 14,
+                        color: const Color(0xFF133E87),
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
                   ],
@@ -257,78 +376,11 @@ class _DashboardScreenState extends State<DashboardScreen> {
               ),
             ],
           ),
-          SizedBox(height: screenHeight * 0.02),
-          Container(
-            padding: EdgeInsets.all(screenWidth * 0.05),
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [Color(0xFF133E87), Color(0xFF1E56B1)],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Total Saldo',
-                      style: TextStyle(
-                          // ignore: deprecated_member_use
-                          color: Colors.white.withOpacity(0.9),
-                          fontSize: isSmallScreen ? 12 : 14),
-                    ),
-                    SizedBox(height: screenHeight * 0.01),
-                    Text(
-                      'Rp0',
-                      style: TextStyle(
-                        fontSize: isSmallScreen ? 20 : 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    elevation: 0,
-                    padding: EdgeInsets.symmetric(
-                        horizontal: isSmallScreen ? 12 : 20,
-                        vertical: isSmallScreen ? 8 : 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(Icons.account_balance_wallet,
-                          color: const Color(0xFF133E87)),
-                      SizedBox(width: screenWidth * 0.02),
-                      Text(
-                        'Dompet',
-                        style: TextStyle(
-                          fontSize: isSmallScreen ? 12 : 14,
-                          color: const Color(0xFF133E87),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
+        ),
+      ],
+    ),
+  );
+}
   Widget _buildMenuCard(IconData icon, String title,
       {required VoidCallback onTap, required BuildContext context}) {
     final screenWidth = MediaQuery.of(context).size.width;
